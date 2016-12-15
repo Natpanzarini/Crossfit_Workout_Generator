3ff067276479eccabb503a6485d24b679f9e35a4
nat panzarini <natpanzarini@gmail.com>
2016-12-15 10:52:15 -0800

    Assignment 6 turn in

--- app/src/main/java/com/example/cisc/crossfitworkoutgenerator/HomeScreen.java
+++ app/src/main/java/com/example/cisc/crossfitworkoutgenerator/HomeScreen.java
@@ -1,45 +1,99 @@
 package com.example.cisc.crossfitworkoutgenerator;
 
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.IOException;
+
 public class HomeScreen extends AppCompatActivity implements View.OnClickListener {
 
-    //Animation wobble = AnimationUtils.loadAnimation(this, R.anim.wobble);
+    private SoundPool soundPool;
+    int sample1 = -1;
+    int sample2 = -1;
+    int sample3 = -1;
+
+    Animation wobble;
+
+    Button gallery_button;
+    Button generate_workout;
+    Button options_menu;
+    Button send_feedback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
-        Button gallery_button = (Button) findViewById(R.id.gallery_button);
+        wobble = AnimationUtils.loadAnimation(this, R.anim.wobble);
+
+        gallery_button = (Button) findViewById(R.id.gallery_button);
         gallery_button.setOnClickListener(this);
 
-        Button generate_workout = (Button) findViewById(R.id.generate_workout);
+        generate_workout = (Button) findViewById(R.id.generate_workout);
         generate_workout.setOnClickListener(this);
 
-        Button options_menu = (Button) findViewById(R.id.options_menu);
+        options_menu = (Button) findViewById(R.id.options_menu);
         options_menu.setOnClickListener(this);
 
-        Button send_feedback = (Button) findViewById(R.id.send_feedback);
+        send_feedback = (Button) findViewById(R.id.send_feedback);
         send_feedback.setOnClickListener(this);
+
+        soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC,0);
+
+        try{
+
+//Create objects of the 2 required classes
+
+            AssetManager assetManager = getAssets();
+
+            AssetFileDescriptor descriptor;
+
+//create our three fx in memory ready for use
+
+            descriptor = assetManager.openFd("gallery.wav");
+
+            sample1 = soundPool.load(descriptor, 0);
+
+            descriptor = assetManager.openFd("send_feedback.wav");
+
+            sample2 = soundPool.load(descriptor, 0);
+
+            descriptor = assetManager.openFd("StartSound.wav");
+
+            sample3 = soundPool.load(descriptor, 0);
+
+        }catch(IOException e){
+
+//catch exceptions here
+
+        }
 
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId())
+        {
             case R.id.gallery_button:
+                soundPool.play(sample1, 1, 1, 0, 0, 1);
                 Intent i;
                 i = new Intent(this, Gallery.class);
                 startActivity(i);
                 break;
 
             case R.id.generate_workout:
+                soundPool.play(sample2, 1, 1, 0, 0, 1);
+                generate_workout.startAnimation(wobble);
                 Toast.makeText(HomeScreen.this, "Coming Soon!", Toast.LENGTH_SHORT).show();
                 break;
 
@@ -49,6 +103,7 @@ public class HomeScreen extends AppCompatActivity implements View.OnClickListene
                 break;
 
             case R.id.send_feedback:
+                soundPool.play(sample3, 1, 1, 0, 0, 1);
                 i = new Intent(this, SendFeedback.class);
                 startActivity(i);
                 break;
--- app/src/main/java/com/example/cisc/crossfitworkoutgenerator/SendFeedback.java
+++ app/src/main/java/com/example/cisc/crossfitworkoutgenerator/SendFeedback.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
 public class SendFeedback extends AppCompatActivity {
 
@@ -14,26 +15,21 @@ public class SendFeedback extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_feedback);
 
+        findViewById(R.id.btn_feedback).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendFeedback();
+            }
+        });
     }
 
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        MenuInflater hardwaremenu = getMenuInflater();
-        hardwaremenu.inflate(R.menu.main_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.send_feedback:
-                Intent Email = new Intent(Intent.ACTION_SEND);
-                Email.setType("text/email");
-                Email.putExtra(Intent.EXTRA_EMAIL, new String[]{"natpanzarini@gmail.com"});
-                Email.putExtra(Intent.EXTRA_SUBJECT, "Feedback");
-                Email.putExtra(Intent.EXTRA_TEXT, "Dear ...," + "");
-                startActivity(Intent.createChooser(Email, "Send Feedback:"));
-        }
-        return true;
+    //Start a new activity for sending a feedback email
+    private void sendFeedback() {
+        final Intent _Intent = new Intent(android.content.Intent.ACTION_SEND);
+        _Intent.setType("text/html");
+        _Intent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{getString(R.string.mail_feedback_email)});
+        _Intent.putExtra(android.content.Intent.EXTRA_SUBJECT, getString(R.string.mail_feedback_subject));
+        _Intent.putExtra(android.content.Intent.EXTRA_TEXT, getString(R.string.mail_feedback_message));
+        startActivity(Intent.createChooser(_Intent, getString(R.string.title_send_feedback)));
     }
 }
